ddae3abb9f6caabdf206b9ec05f06290a94e50f9
Mickey1992 <wl920425@gmail.com>
2021-11-14 14:53:58 +0900

    Solution2 of [387. First Unique Character in a String]
    Check the first and the last index of each lowercase English letters.
    If they are same, means that the letter is unique in String.
    
    Record the minimum index of such letter.

--- src/leetcode/FirstUniqueCharacterInAString.java
+++ src/leetcode/FirstUniqueCharacterInAString.java
@@ -2,9 +2,9 @@ package leetcode;
 
 public class FirstUniqueCharacterInAString {
     public static void main(String[] args){
-        System.out.println(firstUniqChar("leetcode"));
-        System.out.println(firstUniqChar("loveleetcode"));
-        System.out.println(firstUniqChar("aabb"));
+        System.out.println(firstUniqChar2("leetcode"));
+        System.out.println(firstUniqChar2("loveleetcode"));
+        System.out.println(firstUniqChar2("aabb"));
     }
     public static int firstUniqChar(String s) {
         int[] occurrence = new int[26];
@@ -17,5 +17,16 @@ public class FirstUniqueCharacterInAString {
                 return i;
         }
         return -1;
+    }
+
+    public static int firstUniqChar2(String s) {
+        int minIndex = s.length();
+        for(int i = 0; i < 26; i++){
+            int index = s.indexOf('a'+i);
+            if(index < 0) continue;
+            if(index != s.lastIndexOf('a'+i)) continue;
+            minIndex = Math.min(minIndex, index);
+        }
+        return minIndex == s.length() ? -1 : minIndex;
     }
 }
